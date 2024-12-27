9ededbdb42c3b8336d7d827f6689efc1b290113a
Daniel Gather <163533297+DanielGather@users.noreply.github.com>
2024-12-27 14:15:39 +0100

    update

--- classes/endboss.class.js
+++ classes/endboss.class.js
@@ -4,7 +4,7 @@ class Endboss extends MovableObject{
     height = 500;
     y = 0;
 
-    IMAGES_SWIMING = [
+    IMAGES_INTRODUCE = [
         'img/2.Enemy/3 Final Enemy/1.Introduce/1.png',
         'img/2.Enemy/3 Final Enemy/1.Introduce/2.png',
         'img/2.Enemy/3 Final Enemy/1.Introduce/3.png',
@@ -17,21 +17,44 @@ class Endboss extends MovableObject{
         'img/2.Enemy/3 Final Enemy/1.Introduce/10.png'
     ]
 
+    IMAGES_SWIMING = [
+        'img/2.Enemy/3 Final Enemy/2.floating/1.png',        
+        'img/2.Enemy/3 Final Enemy/2.floating/2.png',
+        'img/2.Enemy/3 Final Enemy/2.floating/3.png',
+        'img/2.Enemy/3 Final Enemy/2.floating/4.png',
+        'img/2.Enemy/3 Final Enemy/2.floating/5.png',
+        'img/2.Enemy/3 Final Enemy/2.floating/6.png',
+        'img/2.Enemy/3 Final Enemy/2.floating/7.png',
+        'img/2.Enemy/3 Final Enemy/2.floating/8.png',
+        'img/2.Enemy/3 Final Enemy/2.floating/9.png',
+        'img/2.Enemy/3 Final Enemy/2.floating/10.png',
+        'img/2.Enemy/3 Final Enemy/2.floating/11.png',
+        'img/2.Enemy/3 Final Enemy/2.floating/12.png',
+        'img/2.Enemy/3 Final Enemy/2.floating/13.png'
+
+    ]
+
 
     constructor(){
-        super().loadImage(this.IMAGES_SWIMING[0])
+        super().loadImage(this.IMAGES_INTRODUCE[0])
+        this.loadImages(this.IMAGES_INTRODUCE);
         this.loadImages(this.IMAGES_SWIMING);
         this.x = 500;
         this.animate();
-        console.log(this.IMAGES_SWIMING);
-        console.log(this.x);
         
     }
 
     animate(){
+        let i = 0
         setInterval(()=>{
-            this.playAnimation(this.IMAGES_SWIMING);
-        }, 200)
+            if(i<10){
+                this.playAnimation(this.IMAGES_INTRODUCE);
+                console.log("i", i);
+            } else{
+                this.playAnimation(this.IMAGES_SWIMING);
+            } 
+            i++;
+        }, 150)
     }
 
 }
